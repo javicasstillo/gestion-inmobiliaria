4a44e5b25e8a3bfe3edb18ddb8897c7e0f037009
Javier Castillo <javiercastillo.tuc@gmail.com>
2026-04-14 11:01:18 -0300

    subir

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export default function Sidebar({ page, onNavigate, onLogout, className = '' }) 
   return (
     <div className={`sidebar ${className}`}>
       <div className="sidebar-logo">
-        <h1>Exodo</h1>
+        <h1>Noelia Propiedades</h1>
         <span>Gestión de propiedades</span>
       </div>
 
